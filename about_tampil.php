58e9e0b13b2ae0982b44955ac2a18e1f333817ae
muhammadsofyanblogger <46585390+muhammadsofyanblogger@users.noreply.github.com>
2019-01-25 07:51:42 -0800

    Add files via upload

---
+++ about_tampil.php
@@ -0,0 +1,38 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> About</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <p align="center"><img src="img/logoBSP.jpg"></img></p>
+                            <label class="col-sm-12 control-label"><center><strong>SISTEM INFORMASI PREMI KARYAWAN<br> PT BAKRIE SUMATERA PLANTATIONS </strong></center></label>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Nama Kelompok :
+							 <br>
+							 1. Jihan Qonita Amelia Muntaz (nim : 15220311)
+							 <br> 
+							 2. Muhammad Sofyan Nasution (nim : 15220320)
+							 <br> 
+							 3. Sarah Murni Sinaga (nim : 15220486)
+                            <br><br><br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
